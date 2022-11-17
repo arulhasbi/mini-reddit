f8f94b16d3faf992404df06203217ed96627820f
fakhrul hasbi <fakhrulhasbi@fakhruls-MacBook-Pro.local>
2022-11-17 22:05:52 +0800

    refactor reddit auth

--- src/api/reddit-auth.js
+++ src/api/reddit-auth.js
@@ -1,12 +1,20 @@
 // required parameters for reddit URL
 const response_type = "code";
 export const state = "8477052678";
-const redirect_uri = "http://localhost:3000/callback/";
+let redirect_uri = "";
 const duration = "permanent";
 const scope = "read vote";
 
+if (process.env.NODE_ENV === "development") {
+  redirect_uri = process.env.REACT_APP_CALLBACK_DEV;
+}
+
+if (process.env.NODE_ENV === "production") {
+  redirect_uri = process.env.REACT_APP_CALLBACK_PROD;
+}
+
 export const authorization = () => {
-  const authorizationURL = `https://www.reddit.com/api/v1/authorize?client_id=${client_id}&response_type=${response_type}&state=${state}&redirect_uri=${redirect_uri}&duration=${duration}&scope=${scope}`;
+  const authorizationURL = `https://www.reddit.com/api/v1/authorize?client_id=${process.env.REACT_APP_REDDIT_CLIENT_ID}&response_type=${response_type}&state=${state}&redirect_uri=${redirect_uri}&duration=${duration}&scope=${scope}`;
   window.location.href = authorizationURL;
 };
 
