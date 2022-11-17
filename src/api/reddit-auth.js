ec2f775a157856951ffa1239b8f333c30b8cf6d4
fakhrul hasbi <fakhrulhasbi@fakhruls-MacBook-Pro.local>
2022-11-17 23:22:43 +0800

    added logout button

--- src/api/reddit-auth.js
+++ src/api/reddit-auth.js
@@ -1,4 +1,6 @@
 // required parameters for reddit URL
+let client_id = "";
+let client_secret = "";
 const response_type = "code";
 export const state = "8477052678";
 let redirect_uri = "";
@@ -6,22 +8,24 @@ const duration = "permanent";
 const scope = "read vote";
 
 if (process.env.NODE_ENV === "development") {
+  client_id = process.env.REACT_APP_REDDIT_CLIENT_ID_DEV;
+  client_secret = process.env.REACT_APP_REDDIT_CLIENT_SECRET_DEV;
   redirect_uri = process.env.REACT_APP_CALLBACK_DEV;
 }
 
 if (process.env.NODE_ENV === "production") {
+  client_id = process.env.REACT_APP_REDDIT_CLIENT_ID_PROD;
+  client_secret = process.env.REACT_APP_REDDIT_CLIENT_SECRET_PROD;
   redirect_uri = process.env.REACT_APP_CALLBACK_PROD;
 }
 
 export const authorization = () => {
-  const authorizationURL = `https://www.reddit.com/api/v1/authorize?client_id=${process.env.REACT_APP_REDDIT_CLIENT_ID}&response_type=${response_type}&state=${state}&redirect_uri=${redirect_uri}&duration=${duration}&scope=${scope}`;
+  const authorizationURL = `https://www.reddit.com/api/v1/authorize?client_id=${client_id}&response_type=${response_type}&state=${state}&redirect_uri=${redirect_uri}&duration=${duration}&scope=${scope}`;
   window.location.href = authorizationURL;
 };
 
 export const loadAccessToken = async (code) => {
-  const credentials = btoa(
-    `${process.env.REACT_APP_REDDIT_CLIENT_ID}:${process.env.REACT_APP_REDDIT_CLIENT_SECRET}`
-  );
+  const credentials = btoa(`${client_id}:${client_secret}`);
   const param = new URLSearchParams({
     code: code,
     grant_type: "authorization_code",
